00ac7e130b0f43461f605262ca692590f9eab64a
pouya <pouya@applariat.com>
2017-11-11 18:57:42 -0800

    download URL as input

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,62 +2,18 @@
 
 BB_BRANCH=${BB_BRANCH:-master}
 RUN_TEST=${RUN_TEST}
-
-type=${type}
-project_id=${project_id}
-private_key_id=${private_key_id}
-private_key=${private_key}
-client_email=${client_email}
-client_id=${client_id}
-auth_uri=${auth_uri}
-token_uri=${token_uri}
-auth_provider_x509_cert_url=${auth_provider_x509_cert_url}
-client_x509_cert_url=${client_x509_cert_url}
-
+DOWNLOAD_URL=${DOWNLOAD_URL}
 
 echo Installing required packages, this might take few moments...
 apt-get -qq update
-apt-get -qq install openssl lsb-release wget curl -y
-
-echo Installing google cloud sdk, , this might take few moments...
-export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
-echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
-curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-
-apt-get -qq update
-apt-get -qq install google-cloud-sdk -y
+apt-get -qq install openssl wget curl -y
 
 echo Installing rethinkdb python driver
 pip install rethinkdb
 
-#assemble auth token
-cat << EOF > /root/token.json
-{
-  "type": "$type",
-  "project_id": "$project_id",
-  "private_key_id": "$private_key_id",
-  "private_key": "$private_key",
-  "client_email": "$client_email",
-  "client_id": "$client_id",
-  "auth_uri": "$auth_uri",
-  "token_uri": "$token_uri",
-  "auth_provider_x509_cert_url": "$auth_provider_x509_cert_url",
-  "client_x509_cert_url": "$client_x509_cert_url"
-}
-EOF
-
-#Configuring gsutils
-cat << EOF > /root/.boto
-[Credentials]
-gs_service_key_file = /root/token.json
-[Boto]
-https_validate_certificates = True
-[GoogleCompute]
-[GSUtil]
-content_language = en
-default_api_version = 2
-[OAuth2]
-EOF
+#Downloading and restore backupfile
+wget $DOWNLOAD_URL -O backup.tar.gz
+rethinkdb restore backup.tar.gz -c rethinkdb:28015 --force
 
 mkdir -p /app/apl_common
 
